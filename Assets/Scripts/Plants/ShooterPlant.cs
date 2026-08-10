37184e76ebbdb2c73a8da3a1baa541bc2200b9da
HTNT <huy2609003@gmail.com>
2026-08-10 19:05:49 +0700

    shooterplant

--- Assets/Scripts/Plants/ShooterPlant.cs
+++ Assets/Scripts/Plants/ShooterPlant.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using PVZ_MVS.Scripts.Data;
+using PVZ_MVS.Scripts.Managers;
 
 namespace PVZ_MVS.Scripts.Plants
 {
@@ -8,20 +9,39 @@ namespace PVZ_MVS.Scripts.Plants
         private Transform _currentTarget;
         private float _attackTimer;
         private bool _isInitialized;
+        private bool _hasTargetInLane;
+
+        [SerializeField] private ZombieManager _zombieManager;
+        [SerializeField, Min(0)] private int _lane;
 
         protected ShooterPlantData ShooterData => (ShooterPlantData) Data;
+        protected bool HasTargetInLane => _hasTargetInLane;
+
         // Unity Callback
         protected virtual void Update(){
-            
+            _attackTimer = Mathf.Max(0f, _attackTimer - Time.deltaTime);
+
+            FindTarget();
+            HandleAttack();
         }
 
         // Core Logic
         protected virtual void FindTarget(){
-            
+            if (_zombieManager == null){
+                _hasTargetInLane = false;
+                return;
+            }
+
+            _hasTargetInLane = _zombieManager.HasZombieInLane(_lane);
         }
 
         protected virtual void HandleAttack(){
-            
+            if (!HasTargetInLane || _attackTimer > 0f){
+                return;
+            }
+
+            Shoot();
+            _attackTimer = ShooterData.AttackCooldown;
         }
 
         // Implemented by subclasses
