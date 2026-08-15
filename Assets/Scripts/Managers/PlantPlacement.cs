3e4ccee988c291fee3bdf7c59c439f4134acb193
HTNT <huy2609003@gmail.com>
2026-08-16 01:19:44 +0700

    something for placement

--- Assets/Scripts/Managers/PlantPlacement.cs
+++ Assets/Scripts/Managers/PlantPlacement.cs
@@ -1,7 +1,6 @@
-using UnityEngine;
 using PVZ_MVS.Scripts.Grid;
-using PVZ_MVS.Scripts.Managers;
 using PVZ_MVS.Scripts.Plants;
+using UnityEngine;
 
 namespace PVZ_MVS.Scripts.Managers
 {
@@ -9,24 +8,51 @@ namespace PVZ_MVS.Scripts.Managers
     {
         [SerializeField] private GridManager _gridManager;
         [SerializeField] private ZombieManager _zombieManager;
-        [SerializeField] private Peashooter _peashooterPrefab;
         [SerializeField] private SunManager _sunManager;
+        [SerializeField] private Peashooter _peashooterPrefab;
+        [SerializeField] private SunFlower _sunFlowerPrefab;
+
+        private Plant _selectedPlantPrefab;
+
+        private void Start()
+        {
+            _selectedPlantPrefab = _peashooterPrefab;
+        }
 
         private void Update()
         {
+            HandlePlantSelection();
+
             if (Input.GetMouseButtonDown(0))
             {
-                TryPlacePeashooter();
+                TryPlaceSelectedPlant();
             }
         }
 
-        private void TryPlacePeashooter()
+        private void HandlePlantSelection()
         {
-            int cost = _peashooterPrefab.Data.Cost;
-            if(!_sunManager.TrySpendSun(cost)){
-                Debug.Log("Khong du sun");
+            if (Input.GetKeyDown(KeyCode.Alpha1)
+                || Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                _selectedPlantPrefab = _peashooterPrefab;
+                Debug.Log("Da chon Peashooter");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2)
+                || Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                _selectedPlantPrefab = _sunFlowerPrefab;
+                Debug.Log("Da chon SunFlower");
+            }
+        }
+
+        private void TryPlaceSelectedPlant()
+        {
+            if (_selectedPlantPrefab == null || _gridManager == null || _sunManager == null)
+            {
                 return;
             }
+
             Vector3 mouseWorldPosition =
                 Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -47,17 +73,57 @@ namespace PVZ_MVS.Scripts.Managers
 
             if (cell.IsOccupied)
             {
-                Debug.Log("Da co plant");
                 return;
             }
 
-            Peashooter plant = Instantiate(
-                _peashooterPrefab,
+            if (!HasRequiredManager())
+            {
+                return;
+            }
+
+            int cost = _selectedPlantPrefab.Data.Cost;
+
+            if (!_sunManager.TrySpendSun(cost))
+            {
+                Debug.Log("Khong du sun.");
+                return;
+            }
+
+            Plant plant = Instantiate(
+                _selectedPlantPrefab,
                 cell.WorldPosition,
                 Quaternion.identity);
 
-            plant.Initialize(_zombieManager, row);
+            InitializePlant(plant, row);
+
             cell.SetPlant(plant);
+        }
+
+        private bool HasRequiredManager()
+        {
+            if (_selectedPlantPrefab is ShooterPlant)
+            {
+                return _zombieManager != null;
+            }
+
+            return true;
+        }
+
+        private void InitializePlant(Plant plant, int lane)
+        {
+            if (plant is ShooterPlant shooterPlant)
+            {
+                shooterPlant.Initialize(_zombieManager, lane);
+                return;
+            }
+
+            if (plant is SunFlower sunFlower)
+            {
+                sunFlower.Initialize(_sunManager);
+                return;
+            }
+
+            plant.Initialize();
         }
     }
 }
